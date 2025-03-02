f8ab3507ed8149ba5ff5fd6b0bf2887532a7a5d9
MgKyaw <mgkyaw20112@gmail.com>
2025-03-02 23:46:42 +0700

    feat: Link the component to the html

--- restaurant-menu.component.ts
+++ restaurant-menu.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-restaurant-menu',
   standalone: true,
+  templateUrl: './restaurant-menu.component.html',
 })
 export class AppRestaurantMenu {}
