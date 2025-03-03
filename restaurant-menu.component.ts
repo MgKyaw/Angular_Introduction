4acc6744017c2a1e7dc25d57402eabb549be73ad
MgKyaw <mgkyaw20112@gmail.com>
2025-03-03 23:38:32 +0700

    style: Format ts

--- restaurant-menu.component.ts
+++ restaurant-menu.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   standalone: true,
-  selector: 'app-restaurant-menu',
-  templateUrl: './restaurant-menu.component.html',
-  styleUrl: './restaurant-menu.component.css'
+  selector: "app-restaurant-menu",
+  templateUrl: "./restaurant-menu.component.html",
+  styleUrl: "./restaurant-menu.component.css",
 })
-
-export class RestaurantMenuComponent{
-  restaurantName = 'Gourmet Bistro';
-  logoUrl = 'https://static-assets.codecademy.com/Courses/learn-angular/01-angular-introduction/images/restaurant_logo.png';
+export class RestaurantMenuComponent {
+  restaurantName = "Gourmet Bistro";
+  logoUrl =
+    "https://static-assets.codecademy.com/Courses/learn-angular/01-angular-introduction/images/restaurant_logo.png";
   dishes = [
-    { name: 'Pizza Margherita', description: 'Classic pizza with tomatoes, mozzarella, and basil.', imageUrl: 'https://static-assets.codecademy.com/Courses/learn-angular/01-angular-introduction/images/menu_item_2.jpg' },
-    { name: 'Pasta Carbonara', description: 'Creamy pasta with pancetta, eggs, and cheese.', imageUrl: 'https://static-assets.codecademy.com/Courses/learn-angular/01-angular-introduction/images/menu_item_1.jpg' }
+    {
+      name: "Pizza Margherita",
+      description: "Classic pizza with tomatoes, mozzarella, and basil.",
+      imageUrl:
+        "https://static-assets.codecademy.com/Courses/learn-angular/01-angular-introduction/images/menu_item_2.jpg",
+    },
+    {
+      name: "Pasta Carbonara",
+      description: "Creamy pasta with pancetta, eggs, and cheese.",
+      imageUrl:
+        "https://static-assets.codecademy.com/Courses/learn-angular/01-angular-introduction/images/menu_item_1.jpg",
+    },
   ];
 }
